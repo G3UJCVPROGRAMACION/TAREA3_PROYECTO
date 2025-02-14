ed5d0bb4ad84118ef5d13294827b15c1ffb9dd7a
Juan Carlos Casulá <Juan Carlos Casul@JUAN-CARLOS-TEC>
2025-02-14 01:19:06 -0600

    Agregar archivos de proyecto.

---
+++ TAREA3_PROYECTO/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TAREA3_PROYECTO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //ARREGLOS PARA APROVACION DE PROYECTO
+
+            int[] numeros = new int[7];
+            Console.WriteLine("Ingrese 7 numeros enteros: ");
+            for (int i = 0; i < 7; i++)
+            {
+                numeros[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < 7; i++)
+            {
+                Console.WriteLine($"Elemento {i + 1} Posicion {i} {numeros[i]}");
+            }
+            int[,] matriz = new int[3, 3];
+            Console.WriteLine("Ingrese 9 numeros enteros: ");
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    matriz[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.WriteLine($"Elemento {i + 1} Posicion {i},{j} {matriz[i, j]}");
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
